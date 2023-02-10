c1785ea228667cfccf152732d2ea5092fed0f6f3
Bradley Mubenga <bradleymubenga@Bradleys-MacBook-Pro.local>
2023-02-10 18:32:54 +0200

    Front Fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9d20c8f84a2050e"; }
+/******/ 		__webpack_require__.h = function() { return "9fbf6fee316136b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
